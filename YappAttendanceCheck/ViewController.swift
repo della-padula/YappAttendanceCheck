877ef020b8f7f966074f99911e837f9d7f779935
denny.k <denny.k@kakaocorp.com>
2020-04-12 16:29:58 +0900

    Initial Project Configuration

---
+++ YappAttendanceCheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YappAttendanceCheck
+//
+//  Created by 김태인 on 2020/04/12.
+//  Copyright © 2020 김태인. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
